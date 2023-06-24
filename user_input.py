90e369cd8923320b784ec540c20e339e1588ddd8
darthur1 <40774466+WUNISTUDIOS@users.noreply.github.com>
2023-06-24 13:39:08 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,74 @@
+
+# def user_age_name(name, subject):
+# 	message = 'hello' + name + 'you turn' + subject + 'in a few days'
+# 	print(message)
+
+# test = input("What is your name")
+# age = int(input("how old are you"))
+# year = 2023 - age + 100
+# print (test * ', you will be 100 in the year' + str(year))
+
+
+# num = input("enter a number:")
+# ans = num % 2
+# if ans > 0:
+# 		print("you picked an odd number.")
+
+
+# name = input("user name")
+# age = input("user age")
+# year = name - age
+# print("something")
+
+# a = [1,1,2,3,5,8,13,21,34,55,89]
+# #for element in a:
+# #	print(element)
+
+# for x in a:
+# 		if x < 5:
+# 			print(x)
+
+# my_tuple = ("apple", "banana", "apple", "grape")
+# my_tuple2 = (1,2,3)
+
+# my_tuple3 = my_tuple + my_tuple2
+# print(my_tuple3)
+
+
+# var = int("4")
+# list_range = list(range(1, var+1))
+# dividor_list = []
+
+# for number in list_range:
+# 	if var % number ==0:
+# 		dividor_list.append(number)
+
+# print(dividor_list)
+
+
+# a = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+a = str("testing")
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] 
+
+def palindrome(a):
+	if(a == a[::-1]): 
+		print('is palindrome')
+
+# if int(a in b):
+#     print('duplicate')
+# else: print('nothing found')
+
+
+
+# print(a + list(set(b) - set(a)))
+
+# def add(number_1, number_2):
+#     number_1 + number_2
+#     print(number_1 + number_2)
+
+    
+
+# number_a = int(input("first number"))
+# number_b = int(input("sencond number"))
+# add(number_a, number_b)
+
